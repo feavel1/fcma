ffe013ce3906eba6ba71903f0531cdae2e0f7ca7
Feavel <tanuki20966@gmail.com>
2022-08-15 23:07:16 +0800

    cool

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class:dark>
-	<main class="bg-zinc-300 dark:bg-zinc-800 dark:text-white p-8 min-h-screen ">
+	<main class="bg-zinc-300 dark:bg-zinc-800 dark:text-white p-8 min-h-screen">
 		<Nav bind:dark />
 		<slot class="max-w-6xl mx-auto" />
 	</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,58 +1,89 @@
-<h1 class="font-bold text-3xl my-5">欢迎你来到FCMA官网</h1>
-<p>
-	可以在我们的原版的 <a href="/" class="text-2xl font-bold hover:underline">notion网站</a> 阅读更多资料
-</p>
+<div class="max-w-6xl mx-auto" title="Home">
+	<h1 class="font-bold text-3xl my-5">欢迎你来到FCMA官网</h1>
+	<p>
+		可以在我们的原版的 <a href="/" class="text-2xl font-bold hover:underline">notion网站</a> 阅读更多资料
+	</p>
 
-<h1 class="text-3xl py-8"><a href="/" class="text-cyan-400">F</a>CMA</h1>
+	<div class="flex justify-between py-8">
+		<h1 class="text-3xl "><a href="/" class="text-cyan-400">F</a>CMA</h1>
+		<div class="flex flex-row gap-2">
+			<div class="bg-slate-700 h-10 w-1" />
+			<div class="bg-slate-600 h-10 w-1" />
+			<div class="bg-slate-500 h-10 w-1" />
+			<div class="bg-slate-400 h-10 w-1" />
+			<div class="bg-slate-300 h-10 w-1" />
+			<div class="bg-slate-200 h-10 w-1" />
+			<div class="bg-slate-100 h-10 w-1" />
+		</div>
+	</div>
 
-<hr class="mb-8" />
+	<hr class="mb-8" />
 
-<div class="grid grid-cols-5 content-center">
-	<div class="flex flex-col justify-center gap-3 w-10 ">
-		<div class="bg-slate-700 h-10 rounded-full" />
-		<div class="bg-slate-600 h-10 rounded-2xl" />
-		<div class="bg-slate-500 h-10 rounded-xl" />
-		<div class="bg-slate-400 h-10 rounded-md" />
-		<div class="bg-slate-300 h-10 rounded" />
-		<div class="bg-slate-200 h-10 rounded-sm" />
-		<div class="bg-slate-100 h-10 rounded-none" />
+	<div class="">
+		<div class="grid grid-rows-7 gap-3 ">
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-700 h-10 w-10 rounded-full " />
+				<div class="bg-slate-700 h-10 w-10 rounded-full " />
+				<div class="bg-slate-700 h-10 w-10 rounded-full " />
+				<div class="bg-slate-700 h-10 w-10 rounded-full " />
+				<div class="bg-slate-700 h-10 w-10 rounded-full " />
+			</div>
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-600 h-10 w-10 rounded-2xl " />
+				<div class="bg-slate-600 h-10 w-10 rounded-2xl " />
+				<div class="bg-slate-600 h-10 w-10 rounded-2xl " />
+				<div class="bg-slate-600 h-10 w-10 rounded-2xl " />
+				<div class="bg-slate-600 h-10 w-10 rounded-2xl " />
+			</div>
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-500 h-10 w-10 rounded-xl " />
+				<div class="bg-slate-500 h-10 w-10 rounded-xl " />
+				<div class="bg-slate-500 h-10 w-10 rounded-xl " />
+				<div class="bg-slate-500 h-10 w-10 rounded-xl " />
+				<div class="bg-slate-500 h-10 w-10 rounded-xl " />
+			</div>
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-400 h-10 w-10 rounded-md" />
+				<div class="bg-slate-400 h-10 w-10 rounded-md" />
+				<div class="bg-slate-400 h-10 w-10 rounded-md" />
+				<div class="bg-slate-400 h-10 w-10 rounded-md" />
+				<div class="bg-slate-400 h-10 w-10 rounded-md" />
+			</div>
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-300 h-10 w-10 rounded" />
+				<div class="bg-slate-300 h-10 w-10 rounded" />
+				<div class="bg-slate-300 h-10 w-10 rounded" />
+				<div class="bg-slate-300 h-10 w-10 rounded" />
+				<div class="bg-slate-300 h-10 w-10 rounded" />
+			</div>
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-200 h-10 w-10 rounded-sm" />
+				<div class="bg-slate-200 h-10 w-10 rounded-sm" />
+				<div class="bg-slate-200 h-10 w-10 rounded-sm" />
+				<div class="bg-slate-200 h-10 w-10 rounded-sm" />
+				<div class="bg-slate-200 h-10 w-10 rounded-sm" />
+			</div>
+			<div class="flex flex-row justify-between gap-3">
+				<div class="bg-slate-100 h-10 w-10 rounded-none" />
+				<div class="bg-slate-100 h-10 w-10 rounded-none" />
+				<div class="bg-slate-100 h-10 w-10 rounded-none" />
+				<div class="bg-slate-100 h-10 w-10 rounded-none" />
+				<div class="bg-slate-100 h-10 w-10 rounded-none" />
+			</div>
+		</div>
 	</div>
-	<div class="flex flex-col gap-3 w-10">
-		<div class="bg-slate-700 h-10 rounded-full" />
-		<div class="bg-slate-600 h-10 rounded-2xl" />
-		<div class="bg-slate-500 h-10 rounded-xl" />
-		<div class="bg-slate-400 h-10 rounded-md" />
-		<div class="bg-slate-300 h-10 rounded" />
-		<div class="bg-slate-200 h-10 rounded-sm" />
-		<div class="bg-slate-100 h-10" />
-	</div>
-	<div class="flex flex-col gap-3 w-10">
-		<div class="bg-slate-700 h-10 rounded-full" />
-		<div class="bg-slate-600 h-10 rounded-2xl" />
-		<div class="bg-slate-500 h-10 rounded-xl" />
-		<div class="bg-slate-400 h-10 rounded-md" />
-		<div class="bg-slate-300 h-10 rounded" />
-		<div class="bg-slate-200 h-10 rounded-sm" />
-		<div class="bg-slate-100 h-10" />
-	</div>
-	<div class="flex flex-col gap-3 w-10">
-		<div class="bg-slate-700 h-10 rounded-full" />
-		<div class="bg-slate-600 h-10 rounded-2xl" />
-		<div class="bg-slate-500 h-10 rounded-xl" />
-		<div class="bg-slate-400 h-10 rounded-md" />
-		<div class="bg-slate-300 h-10 rounded" />
-		<div class="bg-slate-200 h-10 rounded-sm" />
-		<div class="bg-slate-100 h-10" />
-	</div>
-	<div class="flex flex-col gap-3 w-10">
-		<div class="bg-slate-700 h-10 rounded-full" />
-		<div class="bg-slate-600 h-10 rounded-2xl" />
-		<div class="bg-slate-500 h-10 rounded-xl" />
-		<div class="bg-slate-400 h-10 rounded-md" />
-		<div class="bg-slate-300 h-10 rounded" />
-		<div class="bg-slate-200 h-10 rounded-sm" />
-		<div class="bg-slate-100 h-10" />
+	<hr class="my-8" />
+
+	<div class="flex justify-between my-8">
+		<div class="flex flex-row gap-2">
+			<div class="bg-slate-700 h-10 w-1" />
+			<div class="bg-slate-600 h-10 w-1" />
+			<div class="bg-slate-500 h-10 w-1" />
+			<div class="bg-slate-400 h-10 w-1" />
+			<div class="bg-slate-300 h-10 w-1" />
+			<div class="bg-slate-200 h-10 w-1" />
+			<div class="bg-slate-100 h-10 w-1" />
+		</div>
+		<h1 class="text-3xl "><a href="/" class="text-cyan-400">F</a>CMA</h1>
 	</div>
 </div>
-
-<hr class="my-8" />
